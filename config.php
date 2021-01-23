57aef6cb3c6ee567d7a89463c8a174c28c3e05cb
rocapa18 <36650467+rocapa18@users.noreply.github.com>
2021-01-23 11:54:58 -0300

    Ultimo cambio en API

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_NAME', 'farinata');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'dbicpakpbkyzgi');
+define('DB_USER', 'um3yj9k6mfcyn');
+define('DB_PASSWORD', 'rcpt4937');
+define('DB_HOST', '35.209.67.183');
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
--- controladores/inicio_Ctrl.php
+++ controladores/inicio_Ctrl.php
@@ -7,13 +7,12 @@ class Inicio_Ctrl
     public function Obtener_Totales($f3)
     {
         $M_Usuario = new M_Usuarios();
-        $M_Pedido = new M_Pedidos();
         $M_Producto = new M_Productos();
         
         echo json_encode([
             'mensaje' => '',
             'info' => [
-                'pedidos' => $M_Pedido->count(),
+                // 'pedidos' => $M_Pedido->count(),
                 'productos' => $M_Producto->count(),
                 'clientes' => $M_Usuario->count(),
             ]
